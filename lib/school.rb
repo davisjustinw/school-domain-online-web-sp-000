20c2ab1d60904536b1e9944eeabbcffdb44eea04
Justin Davis <davisjustinw@gmail.com>
2019-01-10 04:00:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,7 @@ class School
   end
   
   def add_student(student, grade)
-    if @roster.include?(grade)
-      @roster[grade] << student
-    else
-      @roster[grade] = []
+    @roster[grade] ||= []
       @roster[grade] << student
     end
   end
